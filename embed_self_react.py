a8a5e24aadaa2ae5bfb091a6002159c64104f8e6
illumi <github@illumi.moe>
2023-10-26 19:19:03 -0400

    add reaction to embed

---
+++ embed_self_react.py
@@ -0,0 +1,35 @@
+import discord
+import toml
+
+from dotenv import load_dotenv
+
+# Parse config file for token and channel id
+with open('config.toml', 'r') as f:
+    config = toml.load(f)
+    MESSAGE_ID = config['MESSAGE_ID']
+    ROLE_CHANNEL_ID = config['ROLE_CHANNEL_ID']
+    TOKEN = config['TOKEN']
+    
+# Bot setup
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+
+    channel = client.get_channel(ROLE_CHANNEL_ID)
+    # Select desired embed
+    message = await channel.fetch_message(MESSAGE_ID) 
+
+    # Emotes to react with
+    emotes = [
+        '<:valorant:1166902590693462117>',
+        '<:league:1166903222582124604>'
+    ]
+
+    # Add reactions to embed
+    for emote in emotes:
+        await message.add_reaction(emote)
+
+client.run(TOKEN)
